214f80404f2df2bc0424e4e1c9268da9b81d003b
oliversayshi <olivershi2001@gmail.com>
2022-02-25 14:00:53 -0500

    Leaderboard Works

--- hw2/src/App.js
+++ hw2/src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './App.css';
+import Leaderboard from './Leaderboard';
 // import { useNavigate } from 'react-router-dom';
-import quiz from './quiz.json';
+// import quiz from './quiz.json';
 
 function App() {
   // const navigate = useNavigate();
   return (
     <div>
-      <Login questionList={quiz} />
+      <Leaderboard />
     </div>
   );
 }
-
-//Leaderboard leadersList
 
 export default App;
