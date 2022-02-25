f70e76d97e0f50709a3005f7c44a77a6e0c79aa2
oliversayshi <olivershi2001@gmail.com>
2022-02-25 12:58:36 -0500

    Changes

--- hw2/src/App.js
+++ hw2/src/App.js
@@ -2,16 +2,15 @@ import React from 'react';
 import './App.css';
 // import { useNavigate } from 'react-router-dom';
 import quiz from './quiz.json';
-import Quiz from './Quiz';
-// import Login from './Login';
+// import Quiz from './Quiz';
+import Login from './Login';
 
 function App() {
   // const navigate = useNavigate();
   return (
-    <Quiz questionList={quiz} />
-    /* <div>
-      <Login navigation={navigate} questionList={quiz} />
-    </div> */
+    <div>
+      <Login questionList={quiz} />
+    </div>
   );
 }
 
